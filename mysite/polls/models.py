a28085832b5c0fd50fe752ec395c0a659b102f78
Ahmadhossein Yazdani <Ahmadhossein Yazdani>
2018-12-23 19:56:50 +0330

    model is ready but not checked

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,14 +1,37 @@
 from django.db import models
 
+
 # Create your models here.
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    uid = models.IntegerField(primary_key=True)
+    email = models.EmailField(max_length=200)
+
+
+class Poll(models.Model):
+    pollId = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    des = models.CharField(max_length=200, default=None)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.IntegerField(default=0)
+
+
+class Option(models.Model):
+    text = models.CharField(max_length=200)
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    option = models.ForeignKey(Option, on_delete=models.CASCADE)
+
+
+class PollOptionAssociation(models.Model):
+    option = models.ForeignKey(Option, on_delete=models.CASCADE)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+
+
+class Invitation(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
