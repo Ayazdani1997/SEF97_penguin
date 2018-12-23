e0d94d643f42d13e094df5c7f2dec1f6b310bf2d
Ahmadhossein Yazdani <Ahmadhossein Yazdani>
2018-12-23 18:30:29 +0330

    model persisted on postgre

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'penguin',
-        'USER': 'penguinUser',
+        'USER': 'penguinuser',
         'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432',
