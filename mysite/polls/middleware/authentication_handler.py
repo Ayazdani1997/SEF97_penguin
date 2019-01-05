5a85f2ecffc11c03ed20e736ecbc34fbe9860110
Ahmadhossein Yazdani <ahmad.yazdani@ut.ac.ir>
2019-01-05 09:06:45 +0330

    some change in url classification

--- mysite/polls/middleware/authentication_handler.py
+++ mysite/polls/middleware/authentication_handler.py
@@ -1,20 +1,20 @@
 import re
 
-from django.http import HttpResponse, HttpResponseForbidden
+from django.http import HttpResponseForbidden
 from django.urls import path
 
 
 class AuthenticationMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        self.disallowed_urls = [re.compile(r'polls/^(login)')]
+        self.disallowed_urls = [re.compile(r'^polls/$')]
 
     def __call__(self, request):
         response = self.get_response(request)
         return response
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        if request.path in self.disallowed_urls:
+        if any(url.match(path) for url in self.disallowed_urls):
             username = request.COOKIES['username']
             if username is None:
                 return HttpResponseForbidden()
